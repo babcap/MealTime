eba4e2e9d0507104d6c9f175aab661a135f98273
Arthur <takerarthur@gmail.com>
2020-04-15 11:14:30 +0300

    Deleting mealtime

--- MealTime/SceneDelegate.swift
+++ MealTime/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let context = (UIApplication.shared.delegate as? AppDelegate)?.coreDataStack.persistentContainer.viewContext
         
-        vc.conrext = context
+        vc.context = context
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
