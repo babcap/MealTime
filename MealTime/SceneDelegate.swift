338dd6ca94a74820ef815032715d1d6ce25fd333
Arthur <takerarthur@gmail.com>
2020-04-15 10:15:35 +0300

    Adding core data stack

--- MealTime/SceneDelegate.swift
+++ MealTime/SceneDelegate.swift
@@ -17,6 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let nc = window?.rootViewController as! UINavigationController
+        let vc = nc.topViewController as! ViewController
+        
+        let context = (UIApplication.shared.delegate as? AppDelegate)?.coreDataStack.persistentContainer.viewContext
+        
+        vc.conrext = context
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -42,7 +49,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        
+        (UIApplication.shared.delegate as? AppDelegate)?.coreDataStack.saveContext()
     }
 }
 
