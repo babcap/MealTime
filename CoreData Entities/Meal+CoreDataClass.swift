52c7db07842fb1e2521613025700908bc57bbf66
Arthur <takerarthur@gmail.com>
2020-04-15 10:35:23 +0300

    Add model

---
+++ CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Arthur Batyaev on 4/15/20.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ CoreData Entities/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Arthur Batyaev on 4/15/20.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
